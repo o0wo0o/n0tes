b948e90b92ecba49f477e106f8c7889eccd33841
lox <ilia28ch@gmail.com>
2025-06-24 01:29:53 +0400

    0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
+
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://o0wo0o.github.io',
 	integrations: [mdx(), sitemap()],
 });
